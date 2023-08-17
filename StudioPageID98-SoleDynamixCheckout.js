d83fae64e7c1a27f1c2293257f33a7f7f078d4a1
Kevin Veldman <69542501+taconomix@users.noreply.github.com>
2023-08-17 12:45:51 -0400

    Update StudioPageID98-SoleDynamixCheckout.js

--- StudioPageID98-SoleDynamixCheckout.js
+++ StudioPageID98-SoleDynamixCheckout.js
@@ -7,13 +7,13 @@
         Bryce: Ask Katie what this should say.
 ==================================================================*/
     
-    /*var newButtonText = "Confirm and review order"; 
+    var newButtonText = "Confirm and review order"; 
 
     $(document).ready(function() {
 
         $('[id="btnDelivery"]')[0].textContent = newButtonText;
 
-    });*/
+    });
 
 
 
@@ -27,14 +27,14 @@
         -Can we eliminate the scrolling?
 ==================================================================*/
 
-    /*$(document).on('click', '[id="btnDelivery"]', function (e) {
+    $(document).on('click', '[id="btnDelivery"]', function (e) {
         
         if ( $('[id="SelectState"]').children("option:selected").attr('value').length ) {
         
            $('[id="btnSummary"]').click();
            $('[id="referencesField"]').hide();
         }
-    });*/
+    });
 
 
 
@@ -47,9 +47,43 @@
         -Appears to function properly, not tested enough
 ==================================================================*/
     
-    /*$(document).on('click', '[id="btnPrevious"]', function (e) {
+    $(document).on('click', '[id="btnPrevious"]', function (e) {
         
            $('[id="btnChangeAddress"]').click();
            $('[id="referencesField"]').hide();
-    });*/
+    }); 
+
+
+
+
+/*=================================================================
+
+    Add Powered by EBiz Charge to the cart page
+    Reformat part of the checkout process
+    
+=================================================================*/
+
+$(document).ready(function(){
+    $('#summaryWrapper').children('.row').children('.col-sm-12').append('<div class = "row ebiz"</div>');
+    $('.ebiz').append('<div class = "ebimg" style = "float:right"</div>');
+    $('.ebimg').append('<img src="/en/image/getthumbnail/1335?width=135&amp;height=31&amp;s=001" data-id="1335">');
+    $('.ebiz').append('<div class = "ebtxt" style = "float:right"</div>');
+    $('.ebtxt').append('<h6 style="padding-right: 5px;">Powered by</h6>');
+    $('.ebiz').css({'text-align': 'right', 'padding-right': '15px', 'padding-top': '3px'});
+    
+    //put with the other dev one
+    $('.col-sm-7.col-xs-6 h5').css({'padding-left': '175px'});
+    $('.col-sm-5.col-xs-6 h5').css({'padding-right': '10px'});
+});
+
+
+
+
+
+
+
+
+
+
+
 
