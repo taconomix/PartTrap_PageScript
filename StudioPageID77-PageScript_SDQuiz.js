2a0bdd5bb31ddddae1b2661b1dc1a13af2abe87c
Kevin Veldman <69542501+taconomix@users.noreply.github.com>
2023-08-31 15:35:07 -0400

    Enable Landing Pages
    Enable landing pages with new path script. Also change click function triggering changes to conditions buttons.

--- StudioPageID77-PageScript_SDQuiz.js
+++ StudioPageID77-PageScript_SDQuiz.js
@@ -8,6 +8,7 @@
     ChangeDate: 2023/08/30 -KV
 ======================================================================*/
 
+var stkCodePath = '.config-selected-image-container [field-name="stockCodeShoe"]';
 
 /*========================================================
     Condition selection "none" functionality
@@ -16,8 +17,10 @@
 
     //_ Add classes to Condition options
     var specDxBox = $('.config-selection-button.Additional');
+
     specDxBox.find('input').addClass('dx');
-    specDxBox.find('input[name="None"]').removeClass('dx').addClass('noDx');
+    specDxBox.find('input[name="None"]').attr('class','noDx');
+
 
 
     //_ Clear "None" when any condition is selected
@@ -26,21 +29,23 @@
     });
 
 
-
     //_ Clear conditions when "None" is selected
-    $('.noDx').on('click', function (e) { $('.dx:checked').click(); });
+    $('.noDx').on('click', function (e) {
+        if ( $(this).is(':checked') ) $('.dx:checked').click(); 
+    });
 
 
-    //_ Check "None" condition for Sport & Comfort, uncheck for Spec
-    var insoleTypeNextStep = $('.config-first-step-selection span');
-    
-    insoleTypeNextStep.on('click', function (e) {
+
+    //_ Uncheck, hide noDx option for SPEC
+    $('.config-first-step-selection span').on('click', function (e) {
         
-        var selectedType = $('.config-selected-image-container [field-name="stockCodeShoe"]');
-        var isSpec = selectedType.attr('data-value').substring(12) == "SPEC";
+        var isSpec = $(stkCodePath).attr('data-value').substring(12) == "SPEC";
 
         if ( isSpec == $('.noDx').is(':checked') ) 
             $('.noDx').click();
+
+        if ( isSpec ) $('.noDx').hide();
+        else $('.noDx').show();
     });
 
 
@@ -51,18 +56,16 @@
         Changed 2023/08/30
 
         --waiting for approval to enable
-========================================================
-
-    // Remove original on-click function
-    $(document).off('click', '.add-config-to-cart');
+========================================================*/
 
 
-    // New on-click function (mostly copied from original)
+    $(document).off('click', '.add-config-to-cart'); // remove original click function
+
     $(document).on('click', '.add-config-to-cart', function (e) {
 
         // Get config data
         var configData = selectedData.slice();
-        var deviceDescription = $('.config-selected-image-container [field-name="cDeviceCode_c"]').attr('item-descrip');
+        var deviceDescription = $(stkCodePath).attr('item-descrip');
         
         var data = {
             stockCode: stockCodeToAddToBasket,
@@ -75,37 +78,28 @@
         // Post to cart when no errors found
         $.post('/DienenConfiguratorAddToBasket/AddToBasket', data, function (result) {
 
-            if (result) {
+            if ( result ) {
 
-                if (result.Success) {
-
-                    // Construct link to custom pages
-                    var sdPartNum = $('.config-selected-image-container [field-name="stockCodeShoe"]').attr('data-value');
-
-                    var partNum = ['SOLEDYNAMIX-SPEC', 'SOLEDYNAMIX-PERF', 'SOLEDYNAMIX-COMF'];
-                    var linkEnd = ['condition-specific', 'sport', 'comfort'];
-                    var pageId  = [123, 124, 125];
-
-                    var iPartNum = partNum.indexOf( sdPartNum );
-
-                    var redirectLink = pageId[iPartNum] + '/sitename-result-' + linkEnd[iPartNum];
+                if ( result.Success ) {
 
                     // Update basket
-                    alertify.success(result.Message);
+                    alertify.success( result.Message );
                     PT.Sections.Basket.miniBasket.UpdateMiniBasket();
-                    if (isSavedConfig) {
-                        deleteSavedConfig();
-                    }
+                    if ( isSavedConfig ) deleteSavedConfig();
 
-                    // Redirect to custom link
-                    window.location.href = window.location.origin + '/en/page/' + redirectLink;
-                
+
+                    // Custom Redirect
+                    var type = ['SPEC', 'PERF', 'COMF'];
+                    var link = ['condition-specific', 'sport', 'comfort'];
+                    var page = [123, 124, 125];
+
+                    var _i = type.indexOf( $(stkCodePath).attr('data-value').substring(12) );
+
+                    window.location.href = window.location.origin + '/en/page/' + page[_i] + '/sitename-result-' + link[_i];
+
                 } else {
                     alertify.error(result.Message);
                 }
-            
-            } else {
-                alertify.success("Saved");
             }
         });
     });
